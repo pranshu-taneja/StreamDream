934e2353c432923f6b7573735d776b999c585879
pranshu-taneja <pranshu45taneja@gmail.com>
2023-05-12 01:19:08 +0530

    Resolved Direct Redirect Problem🎉

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 import SignIn from "./pages/SignIn";
@@ -31,7 +31,7 @@ function App() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <BrowserRouter>
+        <HashRouter>
           <Navbar />
           <Main>
             <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
@@ -53,7 +53,7 @@ function App() {
               </Routes>
             </Wrapper>
           </Main>
-        </BrowserRouter>
+        </HashRouter>
       </Container>
     </ThemeProvider>
   );
